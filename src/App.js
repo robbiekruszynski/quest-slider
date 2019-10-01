1f45d1e7ac7e4df67e3bf8415f149e3bcd4a0cc4
robbie <robbiekruszynski@gmail.com>
2019-10-01 11:26:25 -0700

    add more css to quest slider

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
 import './App.css';
 export default () => (
-  <Tabs>
-    <TabList>
-      <Tab>1</Tab>
-      <Tab>2</Tab>
-      <Tab>3</Tab>
-      <Tab>4</Tab>
+    <Tabs>
 
-
-    </TabList>
+      <TabList>
+        <div className="container">
+        <Tab>1</Tab>
+        <Tab>2</Tab>
+        <Tab>3</Tab>
+        <Tab>4</Tab>
+      </div>
+      </TabList>
 
     <TabPanel>
       <h2>Quest 1</h2>
@@ -26,4 +27,5 @@ export default () => (
       <h2>Quest4</h2>
     </TabPanel>
   </Tabs>
+
 );
